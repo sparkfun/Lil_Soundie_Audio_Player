e819b43ea3cb14e630ac6b5733da93ed25586bee
Ryan Mortenson <ryan.j.mortenson@gmail.com>
2018-03-03 10:24:40 -0700

    Reformat code

--- Firmware/Lil_Soundie/gpioctrl.c
+++ Firmware/Lil_Soundie/gpioctrl.c
@@ -1,15 +1,15 @@
 #include "system.h"
 
-#include <stdio.h> //Standard io
+#include <stdio.h>  // Standard io
 #include <stdlib.h> // VS_DSP Standard Library
 #include <vs1000.h> // VS1000B register definitions
-#include <vectors.h> // VS1000B vectors (interrupts and services)
-#include <minifat.h> // Read Only Fat Filesystem
+#include <vectors.h>  // VS1000B vectors (interrupts and services)
+#include <minifat.h>  // Read Only Fat Filesystem
 #include <mapper.h> // Logical Disk
 #include <string.h> // memcpy etc
 #include <player.h> // VS1000B default ROM player
-#include <audio.h> // DAC output
-#include <codec.h> // CODEC interface
+#include <audio.h>  // DAC output
+#include <codec.h>  // CODEC interface
 #include <vsNand.h>
 #include <mappertiny.h>
 #include <usb.h>
@@ -19,7 +19,7 @@
 #include "gpioctrl.h"
 
 extern struct CodecServices cs;
-void puthex(u_int16 a);
+void puthex (u_int16 a);
 /*
    We currently allow two GPIO controls:
    1) GPIO_MASK for binary-encoded file number, all-zero means idle.
@@ -47,99 +47,107 @@ void puthex(u_int16 a);
    See system.h for GPIO_NUMBERED_FILES definition
    */
 
-#define GPIO_MASK       0x001f // Sel. file w/ GPIO0 pins in the mask
+#define GPIO_MASK       0x001f  // Sel. file w/ GPIO0 pins in the mask
 //#define GPIO_INVERTED   //default state is HIGH, buttons bring low
 //#define GPIO_PRIORITIES 0x04ff // Sel. file w/ GPIO0 pins, lower has priority
 
 //#define GPIO0_READY_INDICATOR    GPIO0_CLE
 //#define GPIO0_PLAYING_INDICATOR  GPIO0_CLE /*CLE=GPIO0_12*/
 /*Note: CLE is a bad choice, because it does not come to an audio module pin.*/
-#define GPIO0_PLAYING_INDICATOR  (1<<6) //GPIO0_CLE /*CLE=GPIO0_12*/
+#define GPIO0_PLAYING_INDICATOR  (1<<6) // GPIO0_CLE /*CLE=GPIO0_12*/
 
 
 #if defined(GPIO_MASK) && defined(GPIO_PRIORITIES)
 err 'Both GPIO_MASK and GPIO_PRIORITIES can not be defined at the same time'
 #endif
+void GPIOCtrlIdleHook (void)
+{
 
-
-
-void GPIOCtrlIdleHook(void) {
-
-  if (uiTrigger) {
+  if (uiTrigger)
+  {
     uiTrigger = 0;
 
-    /*
-       If you want the default firmware key-controls, use KeyScan();
-       */
+    /* 
+     * If you want the default firmware key-controls, use KeyScan(); */
 
 #ifdef GPIO0_PLAYING_INDICATOR
     /* We do it this way so that the logic can be in this file. */
-    if (player.currentFile != -1) {
+    if (player.currentFile != -1)
+    {
       /* high = playing */
-      PERIP(GPIO0_SET_MASK) = GPIO0_PLAYING_INDICATOR;
-    } else {
+      PERIP (GPIO0_SET_MASK) = GPIO0_PLAYING_INDICATOR;
+    }
+    else
+    {
       /* low = waiting for GPIO */
-      PERIP(GPIO0_CLEAR_MASK) = GPIO0_PLAYING_INDICATOR;
+      PERIP (GPIO0_CLEAR_MASK) = GPIO0_PLAYING_INDICATOR;
     }
 #endif
 
-    /*TURN GPIO into GPIO mode.*/
+    /* TURN GPIO into GPIO mode. */
 #ifdef GPIO_MASK
     /* Direct selection through GPIO pins. */
     {
       register s_int16 mask;
 #ifdef GPIO_INVERTED
-      /*default state is high, button pulls low to GND */
-      PERIP(GPIO0_SET_MASK)   = GPIO_MASK | GPIO0_PULLUPS;
-      PERIP(GPIO0_DDR) |= GPIO_MASK;  //Drive to 1
-      for (mask = 0;mask<10000;mask++)
-        USEX(0);
-      PERIP(GPIO0_DDR) &= ~GPIO_MASK; //To inputs
-      for (mask = 0;mask<10000;mask++)
-        USEX(0);
-      mask = ((PERIP(GPIO0_IDATA) ^ GPIO0_PULLUPS) & GPIO_MASK) ^ GPIO_MASK;
+      /* default state is high, button pulls low to GND */
+      PERIP (GPIO0_SET_MASK) = GPIO_MASK | GPIO0_PULLUPS;
+      PERIP (GPIO0_DDR) |= GPIO_MASK; // Drive to 1
+      for (mask = 0; mask < 10000; mask++)
+        USEX (0);
+      PERIP (GPIO0_DDR) &= ~GPIO_MASK;  // To inputs
+      for (mask = 0; mask < 10000; mask++)
+        USEX (0);
+      mask = ((PERIP (GPIO0_IDATA) ^ GPIO0_PULLUPS) & GPIO_MASK) ^ GPIO_MASK;
 #else
-      /*default state is low, button pulls high to IOVDD*/
-      PERIP(GPIO0_CLEAR_MASK) = GPIO_MASK;
-      PERIP(GPIO0_SET_MASK)   = GPIO_MASK & GPIO0_PULLUPS;
-      PERIP(GPIO0_DDR) |= GPIO_MASK;  //Drive to 0
-      for (mask = 0;mask<10000;mask++)
-        USEX(0);
-      PERIP(GPIO0_DDR) &= ~GPIO_MASK; //To inputs
-      for (mask = 0;mask<10000;mask++)
-        USEX(0);
-      mask = (PERIP(GPIO0_IDATA) ^ GPIO0_PULLUPS) & GPIO_MASK;
+      /* default state is low, button pulls high to IOVDD */
+      PERIP (GPIO0_CLEAR_MASK) = GPIO_MASK;
+      PERIP (GPIO0_SET_MASK) = GPIO_MASK & GPIO0_PULLUPS;
+      PERIP (GPIO0_DDR) |= GPIO_MASK; // Drive to 0
+      for (mask = 0; mask < 10000; mask++)
+        USEX (0);
+      PERIP (GPIO0_DDR) &= ~GPIO_MASK;  // To inputs
+      for (mask = 0; mask < 10000; mask++)
+        USEX (0);
+      mask = (PERIP (GPIO0_IDATA) ^ GPIO0_PULLUPS) & GPIO_MASK;
 
 #endif
-      //putch(mask);
+      // putch(mask);
 
-      if (mask) {
+      if (mask)
+      {
         player.currentFile = player.nextFile = mask - 1;
         player.pauseOn = 0;
-      } else {
+      }
+      else
+      {
         player.currentFile = 0xffffU;
       }
     }
-#endif /*GPIO_MASK*/
+#endif /* GPIO_MASK */
 #ifdef GPIO_PRIORITIES
     /* Prioritized selection through GPIO pins. */
     {
       register u_int16 mask;
-      PERIP(GPIO0_CLEAR_MASK) = GPIO_PRIORITIES;
-      PERIP(GPIO0_SET_MASK)   = GPIO_PRIORITIES & GPIO0_PULLUPS;
-      PERIP(GPIO0_DDR) |= GPIO_PRIORITIES;  //Drive to 0
-      for (mask = 0;mask<10000;mask++)
-        USEX(0);
-      PERIP(GPIO0_DDR) &= ~GPIO_PRIORITIES; //To inputs
-      for (mask = 0;mask<10000;mask++)
-        USEX(0);
-      mask = (PERIP(GPIO0_IDATA) ^ GPIO0_PULLUPS) & GPIO_PRIORITIES;
-      //putch(mask);
-      if (mask) {
+      PERIP (GPIO0_CLEAR_MASK) = GPIO_PRIORITIES;
+      PERIP (GPIO0_SET_MASK) = GPIO_PRIORITIES & GPIO0_PULLUPS;
+      PERIP (GPIO0_DDR) |= GPIO_PRIORITIES; // Drive to 0
+      for (mask = 0; mask < 10000; mask++)
+        USEX (0);
+      PERIP (GPIO0_DDR) &= ~GPIO_PRIORITIES;  // To inputs
+      for (mask = 0; mask < 10000; mask++)
+        USEX (0);
+      mask = (PERIP (GPIO0_IDATA) ^ GPIO0_PULLUPS) & GPIO_PRIORITIES;
+      // putch(mask);
+      if (mask)
+      {
         register s_int16 n = 0, m = GPIO_PRIORITIES;
-        while (m) {
-          if (m & 1) {
-            if (mask & 1) {
+        while (m)
+        {
+          if (m & 1)
+          {
+            if (mask & 1)
+            {
               break;
             }
             n++;
@@ -149,42 +157,53 @@ void GPIOCtrlIdleHook(void) {
         }
         if (
 #if 1 /* one pin per file, lower GPIO numbers have priority */
-            player.currentFile == -1 || n < player.currentFile
+             player.currentFile == -1 || n < player.currentFile
 #else /* one pin per file */
-            n != player.currentFile
+             n != player.currentFile
 #endif
-           ) {
+          )
+        {
           player.currentFile = player.nextFile = n;
           cs.cancel = 1;
           player.pauseOn = 0;
         }
       }
     }
-#endif /*GPIO_PRIORITIES*/
+#endif /* GPIO_PRIORITIES */
 
 #if !defined(GPIO_MASK) && !defined(GPIO_PRIORITIES)
     /* Buttons */
-    //KeyScanNoUSB(0x1f); /* with feature button */
+    // KeyScanNoUSB(0x1f); /* with feature button */
 #endif
 #ifdef USE_POWERBUTTON
-    if (PERIP(SCI_STATUS) & SCISTF_REGU_POWERBUT) {
+    if (PERIP (SCI_STATUS) & SCISTF_REGU_POWERBUT)
+    {
       /* In 2FIN2 switching on the uSD regulator affects powerbutton. */
-      if (keyOld == (SCISTF_REGU_POWERBUT<<4)) {
-        if (++keyOldTime >= 3) { /* about 0.2 sec */
-          PERIP(GPIO0_CLEAR_MASK) = GPIO0_SD_POWER;
-          RealPowerOff();
-        } else {
+      if (keyOld == (SCISTF_REGU_POWERBUT << 4))
+      {
+        if (++keyOldTime >= 3)
+        { /* about 0.2 sec */
+          PERIP (GPIO0_CLEAR_MASK) = GPIO0_SD_POWER;
+          RealPowerOff ();
+        }
+        else
+        {
           keyOldTime = 0;
         }
-      } else {
-        keyOld == (SCISTF_REGU_POWERBUT<<4);
       }
-    } else {
+      else
+      {
+        keyOld == (SCISTF_REGU_POWERBUT << 4);
+      }
+    }
+    else
+    {
       keyOld = 0;
     }
 #endif
 #ifndef DISABLE_USB
-    if (USBIsAttached()) {
+    if (USBIsAttached ())
+    {
       cs.cancel = 1;
       player.pauseOn = 0;
     }
@@ -192,20 +211,21 @@ void GPIOCtrlIdleHook(void) {
   }
 }
 
-void GPIOInit(void) {
-  /*TURN GPIO into GPIO mode.*/
+void GPIOInit (void)
+{
+  /* TURN GPIO into GPIO mode. */
 #ifdef GPIO_MASK
-  PERIP(GPIO0_MODE) &= ~GPIO_MASK;
-  PERIP(GPIO0_DDR)  &= ~GPIO_MASK; /* GPIO0[0:n] into inputs */
-#endif /*GPIO_MASK*/
+  PERIP (GPIO0_MODE) &= ~GPIO_MASK;
+  PERIP (GPIO0_DDR) &= ~GPIO_MASK;  /* GPIO0[0:n] into inputs */
+#endif /* GPIO_MASK */
 #ifdef GPIO_PRIORITIES
-  PERIP(GPIO0_MODE) &= ~GPIO_PRIORITIES;
-  PERIP(GPIO0_DDR)  &= ~GPIO_PRIORITIES; /* GPIO0[0:n] into inputs */
-#endif /*GPIO_PRIORITIES*/
+  PERIP (GPIO0_MODE) &= ~GPIO_PRIORITIES;
+  PERIP (GPIO0_DDR) &= ~GPIO_PRIORITIES;  /* GPIO0[0:n] into inputs */
+#endif /* GPIO_PRIORITIES */
 
 #ifdef GPIO0_PLAYING_INDICATOR
-  PERIP(GPIO0_DDR) |= GPIO0_PLAYING_INDICATOR;
+  PERIP (GPIO0_DDR) |= GPIO0_PLAYING_INDICATOR;
 #endif
-  player.nextFile = -1; //No file is being played when we start
-  player.volume = -24; //Max volume
+  player.nextFile = -1; // No file is being played when we start
+  player.volume = -24;  // Max volume
 }
